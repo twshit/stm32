9ae816afd9dffd0f725ca567a915ea886146aa2d
twshit <twshit@users.noreply.github.com>
2022-05-24 11:40:47 +0800

    、

--- temp/main.c
+++ temp/main.c
@@ -14,7 +14,7 @@
 ************************************************/
 
 /*下面主函数是使用HAL库函数实现控制IO口输出*/
-
+int htoo;
 int main(void)
 {
 
@@ -23,32 +23,87 @@ int main(void)
     Stm32_Clock_Init(RCC_PLL_MUL9); //设置时钟,72M
     delay_init(72);                 //初始化延时函数
     LED_Init();                     //初始化LED
-//		Dac1_Init();				//DAC初始化
+	Dac1_Init();				//DAC初始化
     while (1)
     {
-//					dacval++;
-//					HAL_DAC_SetValue(&DAC1_Handler,DAC_CHANNEL_1,DAC_ALIGN_12B_R,dacval);
-//				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);//LED1 紫色灯亮
-//				delay_ms(500);
-//				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);//LED1 紫色灯灭			
-				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_SET);//LED2 蓝色灯亮
-//				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);//LED2 蓝色灯灭
-//				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);//LED3 绿色灯亮
-//				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);//LED3 绿色灯灭
-//				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);//LED4 黄绿色灯亮
-//				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);//LED4 黄绿色灯灭
-//				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4, GPIO_PIN_SET);//LED5 黄色灯亮
-//				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4, GPIO_PIN_RESET);//LED5 黄色灯灭
-//				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_5, GPIO_PIN_SET);//LED6 橘色灯亮
-//				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_5, GPIO_PIN_RESET);//LED6 橘色灯灭
-//				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);//LED7 红色灯亮
-//				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);//LED7 红色灯灭
-//				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);//LED8 白色灯亮
-//				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);//LED8 白色灯灭
-//HAL_GPIO_WritePin(GPIOC, GPIO_PIN_15, GPIO_PIN_RESET);//led0亮
+		switch (地址码)
+		{
+		case 01:
+			
+			LED_Init;
+			switch (参数)
+			{
+			case 1:
+				LED1=0;
+				break;
+			case 2:
+				LED2=0;
+				break;
+			case 3:
+				LED3=0;
+				break;
+			case 4:
+				LED4=0;
+				break;
+			case 5:
+				LED5=0;
+				break;
+			case 6:
+				LED6=0;
+				break;
+			case 7:
+				LED7=0;
+				break;
+			case 8:
+				LED8=0;
+				break;
+			default:
+				break;
+			}
+			break;
+		case 02:
+			int dac=htoo(参数)；
+			HAL_DAC_SetValue(&DAC1_Handler,DAC_CHANNEL_1,DAC_ALIGN_12B_R,dacval);
+			break;
+		case 03:
+
+			break;
+		case 04:
+			int adc=HAL_DAC_SetValue(&DAC1_Handler,DAC_CHANNEL_1,DAC_ALIGN_12B_R,dacval);
+			temp=(float)adcx*(3.3/4096);
+			printf("%f",adc);
+			break;
+		case 05:
+			int adc=HAL_DAC_SetValue(&DAC1_Handler,DAC_CHANNEL_1,DAC_ALIGN_12B_R,dacval);
+			temp=(float)adcx*(3.3/4096);
+			printf("%f",adc);
+			break;
+		default:
+			break;
+		}
+
     }
 }
-
+int htoo(char a[])
+{
+	int len,t,sum=0;
+	len=strlen(a);
+	for(i=0;i<len;i++)
+	{
+		if(a[i]>='0'&&a[i]<='9'||a[i]>='a'&&a[i]<='f'||a[i]>='A'&&a[i]<='F')
+		{
+			if(a[i]>='0'&&a[i]<='9')
+				t=a[i]-'0';
+			else if(a[i]>='a'&&a[i]<='f')
+				t=a[i]-'a'+10;//十六进制a转化为十进制是10,以此类推到f
+			else
+				t=a[i]-'A'+10;
+			sum=sum*16+t;
+		}
+		
+	}
+	return sum;
+}
 
 /*下面主函数使用位带操作实现：*/
 
